9febd42aa526e1c043b0526aef4d5a9dbd26a6af
jugadev <jugadev@outlook.com>
2022-08-11 15:50:12 +0300

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+public class ProductsController : Controller
+{
+    private readonly ILogger<ProductsController> _logger;
+    private readonly StoreContext _context;
+
+    public ProductsController(ILogger<ProductsController> logger,StoreContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+
+        var products =  await _context.Products.ToListAsync();
+        return Ok(products);
+    }
+ [HttpGet("{id}")]
+    public async  Task<ActionResult<Product>> GetProduct(int id)
+    {
+
+        var product =  await _context.Products.FindAsync(id);
+        return Ok(product);
+    }
+    
+}
