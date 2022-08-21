1a03b1c3df61af71fa28b88edf62d0af06a015a2
Nuri aktaş <nuriaktas@hotmail.com>
2022-08-21 20:25:10 +0300

    file/folder structure

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Product } from "./Product";
+import { Product } from "../models/Product";
 const App = () => {
   const [products, setProducts] = useState<Product[]>([]);
   useEffect(() => {
